cbac73e573c894d5d9ce6501dd7910129a4a8213
Senpai-10 <bmjfdrh@gmail.com>
2021-01-22 08:07:06 +0300

    encrypt function

--- main.py
+++ main.py
@@ -1,9 +1,35 @@
 import os
 
+MORSE_CODE_DICT = { 
+    'A':'.-', 'B':'-...', 
+    'C':'-.-.', 'D':'-..', 'E':'.', 
+    'F':'..-.', 'G':'--.', 'H':'....', 
+    'I':'..', 'J':'.---', 'K':'-.-', 
+    'L':'.-..', 'M':'--', 'N':'-.', 
+    'O':'---', 'P':'.--.', 'Q':'--.-', 
+    'R':'.-.', 'S':'...', 'T':'-', 
+    'U':'..-', 'V':'...-', 'W':'.--', 
+    'X':'-..-', 'Y':'-.--', 'Z':'--..', 
+    '1':'.----', '2':'..---', '3':'...--', 
+    '4':'....-', '5':'.....', '6':'-....', 
+    '7':'--...', '8':'---..', '9':'----.', 
+    '0':'-----', ', ':'--..--', '.':'.-.-.-', 
+    '?':'..--..', '/':'-..-.', '-':'-....-', 
+    '(':'-.--.', ')':'-.--.-'
+} 
+
 # encrypt the string according to the morse code chart 
 def encrypt(msg):
-    ...
+    cipher = '' 
+    for letter in msg.upper(): 
+        if letter != ' ': 
+            
+            cipher += MORSE_CODE_DICT[letter] + ' '
+        else: 
+            cipher += ' '
+    return cipher 
 
 # decrypt the string from morse to english 
 def decrypt(msg):
     ...
+
